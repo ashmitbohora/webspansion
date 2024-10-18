4b4b7c284f3c8402348282e6ba8d5cc8649b0b1a
gamerszzz <gamerashmit@gmail.com>
2024-10-18 17:51:12 -0500

    a

---
+++ script.js
@@ -0,0 +1,72 @@
+
+
+//Question Clicker
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach( question => {
+    question.addEventListener("click",() => {
+
+        const answer = question.nextElementSibling;
+        const rotate = question.querySelector(".showAnswer");
+
+        answer.classList.toggle("active");
+        rotate.classList.toggle("active");
+
+    })
+})
+
+
+
+// Page 2 fade in
+
+
+const stickyElement = document.querySelector(".free-sticky");
+const rightElements = document.querySelectorAll(".page-2-right div");
+
+
+let observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting){
+            entry.target.style.opacity = "1";
+        }
+        else {
+            entry.target.style.opacity = "0";
+        }
+    });
+}, {
+     threshold: 0.5
+});
+
+
+rightElements.forEach(element => {
+
+    observer.observe(element);
+
+});
+
+
+
+
+
+// Nav Mobile
+
+const hamburger = document.querySelector(".fa-bars");
+const xIcon = document.querySelector(".fa-xmark");
+const mobileNav = document.querySelector(".navMobile");
+
+hamburger.addEventListener("click",() => {
+
+    mobileNav.classList.toggle("navActive")
+    mobileNav.classList.toggle("navHidden")
+
+});
+
+xIcon.addEventListener("click",() => {
+
+    mobileNav.classList.toggle("navActive")
+    mobileNav.classList.toggle("navHidden")
+
+});
